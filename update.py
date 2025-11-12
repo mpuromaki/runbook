1c638e93068b7a3c871326c3eee65800c3406450
Marko Puromäki <mpuromaki@gmail.com>
2025-11-12 18:58:25 +0200

    Initial runbook for update

---
+++ update.py
@@ -0,0 +1,11 @@
+"""
+Runbook for updating Runbook
+
+Fetches latest version from Github and upgrades to it.
+"""
+
+step("Fetch latest Runbook version")
+shell("git pull origin main")
+
+step("Upgrade Runbook")
+shell("pip install --upgrade .")
